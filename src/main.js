0738b83e3c70bbf433ed80062ccf6d34e5f95ad1
Patrick Nelson <patnelson@ebay.com>
2023-07-11 16:44:45 -0700

    Using svelte-retag to demo HMR with custom elements in Svelte 4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+import svelteRetag from 'svelte-retag';
 import ExampleElement from './lib/ExampleElement.svelte';
 
 // Pull HTMLElement from .element property.
-customElements.define('example-element', ExampleElement.element);
+//customElements.define('example-element', ExampleElement.element);
+
+// Define the custom element instead via svelte-retag and passing the Svelte component directly.
+svelteRetag({
+	component: ExampleElement,
+	tagname: 'example-element',
+});
